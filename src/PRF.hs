c762c421e803d77baf928492963048502217a077
Nesved <116892085+Nesved@users.noreply.github.com>
2023-05-15 22:10:56 -0500

    Update PRF.hs
    - Comments were added to the code.
    - The successor function was removed.
    - The power function was removed.
    - The double function was added.
    - The predecessor function was added.

--- src/PRF.hs
+++ src/PRF.hs
@@ -1,52 +1,54 @@
 module PRF where
 
+--GHC.Natural allows working with natural numbers by providing the Natural type
 import GHC.Natural (Natural)
 
+--An algebraic data type Nat is defined.
 data Nat = Zero | Succ Nat deriving (Show, Eq)
 
+--primitive recursion function over natural numbers.
 recNat :: a -> (Nat -> a -> a) -> Nat -> a
 recNat z _ Zero = z
 recNat z s (Succ n) = s n (recNat z s n)
 
+-- The function natToNatural is defined, which converts a Nat to a GHC Natural.
 natToNatural :: Nat -> Natural
 natToNatural Zero     = 0
 natToNatural (Succ n) = 1 + natToNatural n
 
+-- The function naturalToNat is defined, which converts a Natural to a Nat.
 naturalToNat :: Natural -> Nat
 naturalToNat 0 = Zero
 naturalToNat n = Succ (naturalToNat(n - 1))
 
--- Implementación de la función sucesor
-succNat :: Nat -> Nat
-succNat n = recNat (Succ Zero) addOne n
+-- Implementation of the double function.
+doubleNat :: Nat -> Nat
+doubleNat = recNat Zero addOne
   where
     addOne :: Nat -> Nat -> Nat
-    addOne _ acc = Succ acc
+    addOne _ acc = Succ (Succ acc)
 
--- Implementación de la función suma
+-- Implementation of the sum function.
 addNat :: Nat -> Nat -> Nat
 addNat n m = recNat m addOne n
   where addOne :: Nat -> Nat -> Nat
         addOne _ acc = Succ acc
 
--- Implementación de la función de identidad
+-- Implementation of the identity function.
 identity :: Nat -> Nat
 identity n = recNat Zero successor n
   where
     successor :: Nat -> Nat -> Nat
     successor _ acc = Succ acc
 
--- Implementación de la función multiplicación
+-- Implementation of the multiplication function.
 mulNat :: Nat -> Nat -> Nat
 mulNat n m = recNat Zero (mulStep m) n
   where
     mulStep :: Nat -> Nat -> Nat -> Nat
     mulStep _ _ acc = addNat m acc
 
--- Implementación de la función potencia
-powNat :: Nat -> Nat -> Nat
-powNat _ Zero = Succ Zero
-powNat n (Succ m) = recNat (Succ Zero) (mulStep n) m
-  where
-    mulStep :: Nat -> Nat -> Nat -> Nat
-    mulStep _ acc _ = mulNat n acc
+-- Implementation of the predecessor function.
+predNat :: Nat -> Nat
+predNat Zero = Zero
+predNat (Succ n) = n
